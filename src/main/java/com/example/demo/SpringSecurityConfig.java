7bc026e89e7b023935be49f87cfe80ab0feedba3
nilesh.devdas@gmail.com <nilesh.devdas@gmail.com>
2019-06-07 13:29:36 +0530

    Secure services

---
+++ src/main/java/com/example/demo/SpringSecurityConfig.java
@@ -0,0 +1,20 @@
+package com.example.demo;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
+
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		auth.inMemoryAuthentication().withUser("user").password("{noop}user123").roles("USER").and().withUser("admin")
+				.password("{noop}admin123").roles("USER", "ADMIN");
+	}
+
+	protected void configure(HttpSecurity http) throws Exception {
+		//http.authorizeRequests().anyRequest().authenticated().and().httpBasic();
+		http.authorizeRequests().anyRequest().authenticated().anyRequest().hasAnyRole("ADMIN").and().formLogin();
+	}
+}
